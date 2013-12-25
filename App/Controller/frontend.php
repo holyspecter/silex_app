e309a176a63f6687590fb06641c9ec527b6b1404
Roma Kliuchko <lenivec17@gmail.com>
2013-12-25 15:27:13 +0200

    native form added

--- App/Controller/frontend.php
+++ App/Controller/frontend.php
@@ -10,19 +10,21 @@ $app->get('/', function() use ($app){
 // Add Controller
 $app->match('/add', function(Request $request) use($app) {
 
-    $form = $app['form.factory']->createBuilder('form')
-        ->add('name')
-        ->add('description')
-        ->add('file', 'file', array(
-            'required' => true,
-        ))
-        ->getForm();
 
-    $form->handleRequest($request);
 
-    if ($form->isValid()) {
+//    $form = $app['form.factory']->createBuilder('form')
+//        ->add('name')
+//        ->add('description')
+//        ->add('file', 'file', array(
+//            'required' => true,
+//        ))
+//        ->getForm();
+//
+//    $form->handleRequest($request);
+//
+//    if ($form->isValid()) {
+//
+//    }
 
-    }
-
-    return $app['twig']->render('add.html', array('form' => $form));
+    return $app['twig']->render('add.html', array());
 });
--- App/config.php
+++ App/config.php
@@ -11,14 +11,18 @@ $app['debug'] = true;
 $app['autoloader'] = $app->share(function()use($loader){return $loader;});
 $app['autoloader']->add("App",ROOT);
 
+// Symfony forms
+$app->register(new Silex\Provider\FormServiceProvider(), array(
+                                                              'translator.messages' => array(),
+                                                         ));
+
 // Twig
 $app->register(new \Silex\Provider\TwigServiceProvider());
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     return $twig;
 }));
 $app['twig.path'] = array(__DIR__.'/../template');
+$app['twig']->addExtension(new Twig_Extensions_Extension_I18n());
 //$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
-
-$app->register(new Silex\Provider\FormServiceProvider());
 
 require_once ROOT.'/App/Controller/frontend.php';
